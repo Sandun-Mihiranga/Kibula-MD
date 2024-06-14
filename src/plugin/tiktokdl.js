f9e000924e3d84419cfe9a1bd2dbacc1d7e863cd
Sandun Mihiranga <101651523+Sandun-Mihiranga@users.noreply.github.com>
2024-06-14 14:13:46 +0530

    Update tiktokdl.js

--- src/plugin/tiktokdl.js
+++ src/plugin/tiktokdl.js
@@ -82,7 +82,7 @@ const tiktokCommand = async (m, Matrix) => {
                 text: "© Powered By Sanfun Mihiranga"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
-                 ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/fbbe1744668b44637c21a.jpg` } }, { upload: Matrix.waUploadToServer })),
+                 ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/71c6019160d3ca010c883.jpg` } }, { upload: Matrix.waUploadToServer })),
                 title: "",
                 gifPlayback: true,
                 subtitle: "",
