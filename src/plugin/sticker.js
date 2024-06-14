979619b6fe162789dd5eec502e4f18b80f2997c4
Sandun Mihiranga <101651523+Sandun-Mihiranga@users.noreply.github.com>
2024-06-14 14:26:04 +0530

    Update sticker.js

--- src/plugin/sticker.js
+++ src/plugin/sticker.js
@@ -7,7 +7,7 @@ const stickerCommand = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
   const packname = "";
-  const author = "𝐄𝐭𝐡𝐢𝐱-𝐌𝐃";
+  const author = "© ꜱᴀɴᴅᴜɴ ᴍɪʜɪʀᴀɴɢᴀ";
 
   const validCommands = ['sticker', 's'];
 
