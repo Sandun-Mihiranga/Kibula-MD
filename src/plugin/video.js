cd8d9fdaccb19be3b75b96875253555b54d577eb
Sandun Mihiranga <101651523+Sandun-Mihiranga@users.noreply.github.com>
2024-06-15 12:27:47 +0530

    Update video.js

--- src/plugin/video.js
+++ src/plugin/video.js
@@ -67,7 +67,7 @@ const video = async (m, Matrix) => {
           try {
             const finalVideoBuffer = Buffer.concat(videoBuffer);
           
-            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿' }, { quoted: m });
+            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by Sandun Mihiranga' }, { quoted: m });
             await m.React("✅");
           } catch (err) {
             console.error('Error sending video:', err);
